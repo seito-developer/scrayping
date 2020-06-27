259a3089522dcf25b817816601a2f845140f5547
seito <seitokeri03@yahoo.co.jp>
2020-06-28 02:17:32 +0900

    removed comments

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const rp = require('request-promise');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const jsonfile = require('jsonfile');
 
 const KEYWORD = 'ポケモン';
 const TARGET_URL = 'https://www.famitsu.com/search/?category=new-article&page=';
 let pageCount = 1;
 let articleArray = [];
 
-const getData = new Promise((htmlString, callback) => {
+const PAGES_LEN = 20;
+
+const file = './public/data.json';
+
+
+const getData = (htmlString) => {
 	const htmlDom = new JSDOM(htmlString);
 	const $title = htmlDom.window.document.getElementsByClassName('card__title');
 	const articleLen = $title.length;
-	
-  	let index = 0;
+
+ 	let index = 0;
 
   	while(index < articleLen){
   		let $target = $title[index].getElementsByTagName('a')[0];
@@ -23,36 +29,32 @@ const getData = new Promise((htmlString, callback) => {
 
 		if(obj.title.indexOf(KEYWORD) !== -1){
 			articleArray.push(obj);
-		}	  		
-  		
+		}
   		index++;
   	}
-
-  	
-  		callback();
-  		
-});
-
-getData.then(() => {
-	if(pageCount < 10){
-		pageCount++;
-		getData
-	} else {
-		console.log(articleArray);
-	}
-});
-
-const crawlPages = (url) => {
-	rp.get(url)
-		.then((htmlString) => {
-			getData(htmlString, url);
-		})
-		.catch((error) => {
-	    	console.error('Error:', error);
-	  	});
 };
 
-crawlPages(TARGET_URL + pageCount);
+const foo = () => {
+	console.log('hi');
+	return new Promise((resolve) => {
+		while(pageCount < PAGES_LEN){
+			rp.get(TARGET_URL + pageCount)
+				.then((htmlString) => {
+					getData(htmlString);
+				})
+				.catch((error) => {
+			    	console.error('Error:', error);
+			  	});
+			// crawlPages(pageCount);
+			// console.log('articleArray', articleArray);
+			pageCount++;
+		}
+	});
+};
 
-
-
+const onFulfilled = (value) => {
+	console.log('articleArray', value);	
+};
+// bar();
+foo().then(onFulfilled(articleArray));
+	
